b9e453546db8e48d7f27156e3891ffc1f524d2df
rcornacchia <rlc2160@columbia.edu>
2016-12-22 10:04:51 -0500

    Added CounterDemo to test Counter class

--- algos-in-java/ch1/CounterDemo.java
+++ algos-in-java/ch1/CounterDemo.java
@@ -1,8 +1,18 @@
 public class CounterDemo {
     public static void main(String[] args) {
-        Counter c = new Counter();
+        Counter c;
         System.out.println(c.getCount());
+        c = new Counter();
         c.increment();
-        System.out.println(c.getCount());
+        c.increment(3);
+        int temp = c.getCount();
+        c.reset();
+        Counter d = new Counter(5);
+        d.increment();
+        Counter e = d;
+        temp = e.getCount();
+        e.increment(2);
+        System.out.println(d.getCount());
+        System.out.println(e.getCount());
     }
 }
